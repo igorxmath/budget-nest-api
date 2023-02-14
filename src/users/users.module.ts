3bb44a9536000852fc25f3b5a08aa0beab56667d
Igor Matheus <pessoal@igormatheus.com.br>
2023-02-13 21:55:16 -0300

    Add methods in the user service Now the endpoint returns the list of users and their budget

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { ProductsService } from 'src/products/products.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, ProductsService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { ProductsService } from 'src/products/products.service';
 
+type User = {
+  id: number;
+  name: string;
+  tax: number;
+};
 @Injectable()
 export class UsersService {
-  findAll() {
-    return 'This action returns all users';
+  constructor(private readonly productsService: ProductsService) {}
+
+  async findAll(): Promise<User[]> {
+    return await this.fetchData();
   }
 
-  findById(id: number) {
-    return `This action returns a #${id} user`;
+  async findById(id: number): Promise<User> {
+    return this.fetchData().then((data: User[]) =>
+      data.find((user: User) => user.id === id),
+    );
   }
 
-  setBudget(id: number, products: Array<number>) {
-    return `This action sets a #${id} user's budget to ${products}`;
+  async setBudget(id: number, productsId: Array<number>) {
+    const listOfProducts = await this.productsService.findAll();
+    const user = await this.findById(id);
+    const products = listOfProducts.filter((product) =>
+      productsId.includes(product.id),
+    );
+    const total = products.reduce((acc, product) => acc + product.price, 0);
+    const tax = total * (1 + user.tax / 100);
+    const roundedTax = Math.round(tax * 100) / 100;
+    return { total, tax: roundedTax };
+  }
+
+  private async fetchData(): Promise<User[]> {
+    const response = await fetch(`${process.env.API_URL}/users`);
+    return await response.json();
   }
 }
