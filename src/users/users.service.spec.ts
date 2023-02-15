43786491827ac29da93dde562f299def999f48e4
Igor Matheus <pessoal@igormatheus.com.br>
2023-02-15 18:32:39 -0300

    Added unit tests Tests will be done with the standard jest library

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,66 @@
+import { HttpException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from './users.service';
+import { ProductsService } from '../products/products.service';
+import { ConfigModule } from '@nestjs/config';
+
+describe('UsersService', () => {
+  let usersService: UsersService;
+  let productsService: ProductsService;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
+      providers: [UsersService, ProductsService],
+    }).compile();
+
+    usersService = module.get<UsersService>(UsersService);
+    productsService = module.get<ProductsService>(ProductsService);
+  });
+
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      const users = [
+        { id: 1, name: 'Alice', tax: 10 },
+        { id: 2, name: 'Bob', tax: 15 },
+      ];
+      jest.spyOn(usersService, 'findAll').mockResolvedValue(users);
+      const result = await usersService.findAll();
+      expect(Array.from(result)).toEqual(users);
+    });
+  });
+
+  describe('findById', () => {
+    it('should return a user with a matching ID', async () => {
+      const user = await usersService.findById(1);
+      expect(user.id).toEqual(1);
+    });
+
+    it('should throw an HTTP exception when a user is not found', async () => {
+      await expect(usersService.findById(999)).rejects.toThrow(HttpException);
+    });
+  });
+
+  describe('setBudget', () => {
+    it("should calculate the total price of the products with the user's tax applied", async () => {
+      jest.spyOn(productsService, 'findAll').mockResolvedValue([
+        { id: 1, name: 'Product 1', price: 10 },
+        { id: 2, name: 'Product 2', price: 20 },
+      ]);
+
+      const result = await usersService.setBudget(1, [1, 2]);
+
+      expect(result.totalPrice).toBeCloseTo(53.7);
+    });
+
+    it('should throw an HTTP exception when a product is not found', async () => {
+      jest
+        .spyOn(productsService, 'findAll')
+        .mockResolvedValue([{ id: 1, name: 'Product 1', price: 10 }]);
+
+      await expect(usersService.setBudget(1, [1, 2])).rejects.toThrow(
+        HttpException,
+      );
+    });
+  });
+});
