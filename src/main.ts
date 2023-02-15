1f2962cc213fcd4e3db381e5e067fa27d1e3f5e2
Igor Matheus <pessoal@igormatheus.com.br>
2023-02-15 17:48:04 -0300

    Filter for error handling has been added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './exceptions/http-exception.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
